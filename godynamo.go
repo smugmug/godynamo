998172dd8748c5ac9da80aa4f14da33477557942
Don <don.holly@gmail.com>
2014-03-16 21:49:54 -0700

    Added conf_file path 'config/'

--- godynamo.go
+++ godynamo.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013,2014 SmugMug, Inc. All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -9,7 +9,7 @@
 //       copyright notice, this list of conditions and the following
 //       disclaimer in the documentation and/or other materials provided
 //       with the distribution.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY SMUGMUG, INC. ``AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -27,22 +27,22 @@ package godynamo
 
 import (
 	"fmt"
-	put_item "github.com/smugmug/godynamo/endpoints/put_item"
-	get_item "github.com/smugmug/godynamo/endpoints/get_item"
-	update_item "github.com/smugmug/godynamo/endpoints/update_item"
-	update_table "github.com/smugmug/godynamo/endpoints/update_table"
+	"github.com/smugmug/godynamo/conf"
+	"github.com/smugmug/godynamo/conf_file"
+	conf_iam "github.com/smugmug/godynamo/conf_iam"
+	batch_get_item "github.com/smugmug/godynamo/endpoints/batch_get_item"
+	batch_write_item "github.com/smugmug/godynamo/endpoints/batch_write_item"
+	create "github.com/smugmug/godynamo/endpoints/create_table"
 	delete_item "github.com/smugmug/godynamo/endpoints/delete_item"
 	delete_table "github.com/smugmug/godynamo/endpoints/delete_table"
 	describe_table "github.com/smugmug/godynamo/endpoints/describe_table"
+	get_item "github.com/smugmug/godynamo/endpoints/get_item"
 	list_tables "github.com/smugmug/godynamo/endpoints/list_tables"
-	batch_write_item "github.com/smugmug/godynamo/endpoints/batch_write_item"
-	batch_get_item "github.com/smugmug/godynamo/endpoints/batch_get_item"
-	create "github.com/smugmug/godynamo/endpoints/create_table"
+	put_item "github.com/smugmug/godynamo/endpoints/put_item"
 	query "github.com/smugmug/godynamo/endpoints/query"
 	scan "github.com/smugmug/godynamo/endpoints/scan"
-	conf_iam "github.com/smugmug/godynamo/conf_iam"
-	"github.com/smugmug/godynamo/conf"
-	"github.com/smugmug/godynamo/conf_file"
+	update_item "github.com/smugmug/godynamo/endpoints/update_item"
+	update_table "github.com/smugmug/godynamo/endpoints/update_table"
 )
 
 // This program serves only to include all of the libraries in GoDynamo so that you can
@@ -58,12 +58,12 @@ func installAll() {
 	// deal with iam, or not
 	iam_ready_chan := make(chan bool)
 	go conf_iam.GoIAM(iam_ready_chan)
-	_ = <- iam_ready_chan
+	_ = <-iam_ready_chan
 
-        var get1 get_item.Request
-        var put1 put_item.Request
-        var up1 update_item.Request
-        var upt1 update_table.Request
+	var get1 get_item.Request
+	var put1 put_item.Request
+	var up1 update_item.Request
+	var upt1 update_table.Request
 	var del1 delete_item.Request
 	var delt1 delete_table.Request
 	var batchw1 batch_write_item.Request
@@ -73,7 +73,6 @@ func installAll() {
 	var scan1 scan.Request
 	var desc1 describe_table.Request
 	var list1 list_tables.Request
-	fmt.Printf("%v%v%v%v%v%v%v%v%v%v%v%v%v",get1,put1,up1,upt1,del1,batchw1,batchg1,create1,delt1,query1,scan1,desc1,list1)
-
+	fmt.Printf("%v%v%v%v%v%v%v%v%v%v%v%v%v", get1, put1, up1, upt1, del1, batchw1, batchg1, create1, delt1, query1, scan1, desc1, list1)
 
 }
